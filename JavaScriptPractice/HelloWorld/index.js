78dd5d264a833987414a5c7cb708eb7a7b3b4c7b
Brian J. Lee <bjhlee@gmail.com>
2025-04-08 12:17:10 -0400

    Changed Hello World by linking to id = output

--- JavaScriptPractice/HelloWorld/index.js
+++ JavaScriptPractice/HelloWorld/index.js
@@ -4,3 +4,13 @@ let sum = (a,b) => a + b;
 console.log(sum(3,5)); // 8
 
 document.getElementById("output").textContent = "Hello World from JavaScript!";
+
+// document: Access the document object (represents the entire HTML page)
+
+// .getElementById("output")
+// getElementByID: Find the HTML element with the id "output"
+
+// .textContent = "Hello World from JavaScript!";
+// Set the text content of that element to a new string
+
+
